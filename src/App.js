f24d626647cc4d133f00175605a9eb53c84b6597
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-26 23:54:20 +0300

    trial product frontend view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
+import ProductCard from './components/products/ProductCard';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>      
+      <ProductCard/>
     </div>
   );
 }
--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -1,1 +1,34 @@
-// import ProductCardCSS from './ProductCard.module.css' 
+import ProductCardCSS from "./ProductCard.module.css";
+import React from "react";
+
+function ProductCard() {
+  return (
+    <div className="column is-4">
+      <div className={ProductCardCSS.card}>
+        <div className={ProductCardCSS.image}>
+          <img src="#" alt="ProductName" />
+        </div>
+
+        <div className={ProductCardCSS.companyDetails}>
+          <div className={ProductCardCSS.companyCategory}>Apartment</div>
+          <div className={ProductCardCSS.companyTitle}>Maridadi Home</div>
+          <div className={ProductCardCSS.companyLocation}>
+            1425 San Pedri St. Los Angeles, CA 90015
+          </div>
+          <div className={ProductCardCSS.details}>
+            <div>
+                <p>Beds:<span>4</span></p>
+            </div>
+            <div>
+                <p>Baths:<span>4</span></p>
+            </div>
+            <div>
+                <p>SqFt:<span>4</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default ProductCard;
