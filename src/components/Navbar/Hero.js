973f2009b433458ba12e5813190af83229adfa83
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-26 16:18:48 +0300

    creates background wave

---
+++ src/components/Navbar/Hero.js
@@ -0,0 +1,1 @@
+import HeroCSS from './Hero'
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,29 @@
-import NavbarCSS from './Navbar.module.css'
+import NavbarCSS from "./Navbar.module.css";
 import React from "react";
 
 function Navbar() {
   return (
-  <nav className={NavbarCSS.navHero}>
-    {/* navbar  */}
-        <div className={NavbarCSS.navbar}>
-            <div className={NavbarCSS.navbarItems}>
-                <ul>
-                    <li>Home</li>
-                    <li>AboutUs</li>
-                    <li>ContactUs</li>
-                    
-                </ul>
-            </div>
-            </div>
-  </nav>
-  )
+    <nav className={NavbarCSS.navHero}>
+      {/* navbar  */}
+      <div className={NavbarCSS.navbar}>
+        <div className={NavbarCSS.navbarItems}>
+          <ul>
+            <li>Home</li>
+            <li>AboutUs</li>
+            <li>ContactUs</li>
+          </ul>
+        </div>
+        <div className={NavbarCSS.navbarItems}>logo</div>
+        <div className={NavbarCSS.navbarItems}>
+          <ul>
+            <li>Home</li>
+            <li>AboutUs</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
