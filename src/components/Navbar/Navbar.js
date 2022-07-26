761019c821fc473e0ab7e976d7b2d01220b790f4
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-26 13:26:42 +0300

    creates  navbar and styles

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import NavbarCSS from './Navbar.module.css'
+import React from "react";
+
+function Navbar() {
+  return (
+  <nav className={NavbarCSS.navHero}>
+    {/* navbar  */}
+        <div className={NavbarCSS.navbar}>
+            <div className={NavbarCSS.navbarItems}>
+                <ul>
+                    <li>Home</li>
+                    <li>AboutUs</li>
+                    <li>ContactUs</li>
+                    
+                </ul>
+            </div>
+            </div>
+  </nav>
+  )
+}
+
+export default Navbar;
