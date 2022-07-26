7d5cab342ceca542041d12cb503f29edd9eb7ba2
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-26 20:21:14 +0300

    creates hero page

--- src/components/Navbar/Hero.js
+++ src/components/Navbar/Hero.js
@@ -1,1 +1,25 @@
-import HeroCSS from './Hero'
+import HeroCSS from "./Hero.module.css";
+
+import React from "react";
+import image from "../../logo.svg"
+
+function Hero() {
+  return (
+    <header className={HeroCSS.section + " " + HeroCSS.hero}>
+      <div className="columns">
+        <div className="column is-6">
+          <h1 className={HeroCSS.title}>
+          EVERYONE CAN OWN A HOUSE ONLY WITH BRIGHTLINE
+          </h1>
+          <div>search</div>
+        </div>
+        <div className="column is-6">
+          <div className={HeroCSS.image}>
+            <img alt="new" src={image}></img>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+export default Hero;
