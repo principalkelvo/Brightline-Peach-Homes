317cb45ee5032dd8cf368d8861fdcaf62249d374
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-27 16:36:16 +0300

    creates all products page

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,1 +1,14 @@
 // import ProductsCSS from './Products.module.css' 
+import React from 'react'
+
+import SearchBar from '../Navbar/SearchBar'
+import ProductCard from "./ProductCard"
+function Products(){
+    return(
+        <section>
+         <SearchBar/>
+        <ProductCard/>
+        </section>
+    )
+}
+export default Products;
