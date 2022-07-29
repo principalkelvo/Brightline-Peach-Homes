79ab5e390ce22f7e93fcb7f844a8fad439f797af
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-29 20:35:05 +0300

    fetchs data from db

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import Hero from "./components/home/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Products from "./components/products/Products";
 
 function App() {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
+  const fetchHomes = () =>{
     fetch("http://localhost:4000/homes")
       .then((r) => r.json())
-      .then((homes) => {
-        setData(homes);
-      });
+      .then((homes) => setData(homes));
+  }
+  //add useEffect to setData
+  useEffect(() => {
+    fetchHomes()
   }, []);
-  console.log("data", data);
+  console.log("homes",data);
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Products />} />
-        <Route exact path="/buying" element={<Products />} />
+        <Route exact path="/" element={<Hero />} />
+        <Route exact path="/buying" element={<Products estates={data} />} />
         {/* <Route exact path="/selling" element={<Products />} />
         <Route exact path="/renting" element={<Products />} />
         <Route exact path="/about" element={<Products />} />
-        <Route exact path="/resources" element={<Products />} />
         <Route exact path="/contact" element={<Products />} /> */}
       </Routes>
     </div>
