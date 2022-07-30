85362cdd97a350e8b8b61f84e6bd2e3b8b02bc5c
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-31 00:21:28 +0300

    working filter for product cards

--- src/App.js
+++ src/App.js
@@ -5,30 +5,56 @@ import { Route, Routes } from "react-router-dom";
 import Hero from "./components/home/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Products from "./components/products/Products";
+import ProductView from "./components/products/ProductView";
 
 function App() {
   const [data, setData] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeFilter, setactiveFilter] = useState(false);
 
-  const fetchHomes = () =>{
+  const fetchHomes = () => {
     fetch("http://localhost:4000/homes")
       .then((r) => r.json())
       .then((homes) => setData(homes));
-  }
+  };
+  useEffect(() => {
+    setFiltered(data);
+  }, [data]);
   //add useEffect to setData
   useEffect(() => {
-    fetchHomes()
+    fetchHomes();
   }, []);
-  console.log("homes",data);
+
+  console.log("homes", data);
+  console.log("filtered", filtered);
+
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Hero />} />
-        <Route exact path="/buying" element={<Products estates={data} />} />
+        <Route exact path="/" element={<Hero estates={filtered} />} />
+        <Route
+          exact
+          path="/buying"
+          element={
+            <Products
+              data={data}
+              estates={filtered}
+              setFiltered={setFiltered}
+              activeFilter={activeFilter}
+              setactiveFilter={setactiveFilter}
+            />
+          }
+        />
         {/* <Route exact path="/selling" element={<Products />} />
         <Route exact path="/renting" element={<Products />} />
         <Route exact path="/about" element={<Products />} />
         <Route exact path="/contact" element={<Products />} /> */}
+        <Route
+          exact
+          path="/product/:id"
+          element={<ProductView estates={data} />}
+        />
       </Routes>
     </div>
   );
