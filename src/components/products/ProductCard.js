840c75b06f9a745f025fcde2d12210f327a2d127
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-31 00:20:57 +0300

    working filter

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -1,72 +1,90 @@
 import ProductCardCSS from "./ProductCard.module.css";
 // import image from "../../logo.svg";
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
-function ProductCard({ estates }) {
-  console.log(estates);
+function ProductCard({ estates, setFiltered, activeFilter, data }) {
+  useEffect(() => {
+    if (activeFilter === true) {
+      setFiltered(data);
+      return;
+    }
+    const featured = estates.filter((estate) => estate.featured === true);
+    setFiltered(featured);
+    console.log("featured", featured);
+  }, [activeFilter]);
+
   const estatesCard = estates.map((estate) => (
-    <div  key={estate.id}>
-      <div className={ProductCardCSS.card}>
-        <div className={ProductCardCSS.image}>
-          <div className={ProductCardCSS.type}>
-            For {estate.forSale ? "Sale" : "Rent"}
+    <div className={ProductCardCSS.fullCard} key={estate.id}>
+      <Link className={ProductCardCSS.link} to={`/product/${estate.id}`}>
+        <div className={ProductCardCSS.card}>
+          <div className={ProductCardCSS.image}>
+            <div
+              className={
+                estate.forSale
+                  ? ProductCardCSS.type
+                  : ProductCardCSS.rentType + " " + ProductCardCSS.type
+              }
+            >
+              For {estate.forSale ? "Sale" : "Rent"}
+            </div>
+            <div className={ProductCardCSS.price}>
+              <p>
+                Ksh {estate.forSale ? estate.price.sale : estate.price.rent}
+                <span>/mo</span>
+              </p>
+            </div>
+            <img src={estate.image} alt={estate.title} />
           </div>
-          <div className={ProductCardCSS.price}>
-            <p>
-              Ksh {estate.forSale ? estate.price.sale : estate.price.rent}
-              <span>/mo</span>
-            </p>
-          </div>
-          <img src={estate.image} alt={estate.title} />
-        </div>
 
-        <div className={ProductCardCSS.companyDetails}>
-          <div className={ProductCardCSS.companyCategory}>
-            {estate.category}
-          </div>
-          <div className={ProductCardCSS.companyTitle}>
-            <h1 className={ProductCardCSS.title}>{estate.title}</h1>
-          </div>
-          <div className={ProductCardCSS.companyLocation}>
-            {estate.location.street} {estate.location.city}{" "}
-            {estate.location.country}
-          </div>
-          <div className={ProductCardCSS.details}>
-            <div>
-              <p>
-                Beds:<span>4</span>
-              </p>
+          <div className={ProductCardCSS.companyDetails}>
+            <div className={ProductCardCSS.companyCategory}>
+              {estate.category}
             </div>
-            <div>
-              <p>
-                Baths:<span>4</span>
-              </p>
+            <div className={ProductCardCSS.companyTitle}>
+              <h1 className={ProductCardCSS.title}>{estate.title}</h1>
             </div>
-            <div>
-              <p>
-                SqFt:<span>{estate.size}</span>
-              </p>
+            <div className={ProductCardCSS.companyLocation}>
+              {estate.location.street} {estate.location.city}{" "}
+              {estate.location.country}
+            </div>
+            <div className={ProductCardCSS.details}>
+              <div>
+                <p>
+                  Beds:<span>4</span>
+                </p>
+              </div>
+              <div>
+                <p>
+                  Baths:<span>4</span>
+                </p>
+              </div>
+              <div>
+                <p>
+                  SqFt:<span>{estate.size}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className={ProductCardCSS.owner}>
+            <div className={ProductCardCSS.ownerDetails}>
+              <div className={ProductCardCSS.profileImage}>
+                <img
+                  src={estate.owner.profilePic}
+                  alt={estate.owner.firstName + " " + estate.owner.lastName}
+                />
+              </div>
+              <div className={ProductCardCSS.name}>
+                {estate.owner.firstName + " " + estate.owner.lastName}
+              </div>
+            </div>
+            <div className={ProductCardCSS.dateRange}>
+              {estate.date.range} days ago
             </div>
           </div>
         </div>
-        <hr />
-        <div className={ProductCardCSS.owner}>
-          <div className={ProductCardCSS.ownerDetails}>
-            <div className={ProductCardCSS.profileImage}>
-              <img
-                src={estate.owner.profilePic}
-                alt={estate.owner.firstName + " " + estate.owner.lastName}
-              />
-            </div>
-            <div className={ProductCardCSS.name}>
-              {estate.owner.firstName + " " + estate.owner.lastName}
-            </div>
-          </div>
-          <div className={ProductCardCSS.dateRange}>
-            {estate.date.range} days ago
-          </div>
-        </div>
-      </div>
+      </Link>
     </div>
   ));
   return (
