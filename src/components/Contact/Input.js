ed80a259bcb6d312d80e1978fbf1983173ba1c8a
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-08-01 10:48:42 +0300

    styles up the inputs

--- src/components/Contact/Input.js
+++ src/components/Contact/Input.js
@@ -10,9 +10,10 @@ function Input({ id, type, name, value, onChange, label }) {
         name={name}
         value={value}
         onChange={(e) => onChange(e)}
+        required
       />
       <span className={InputCSS.highlight}></span>
-      <span className={InputCSS.highlight}></span>
+      <span className={InputCSS.bar}></span>
       <label htmlFor={id}>{label}</label>
     </div>
   );
