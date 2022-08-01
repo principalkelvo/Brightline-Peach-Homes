7e95392ab40ef44ce710f663871f5a083ed73eb3
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-08-01 18:54:45 +0300

    input

--- src/components/Navbar/SearchBar.js
+++ src/components/Navbar/SearchBar.js
@@ -1,14 +1,26 @@
 import React from "react";
 import SearchBarCSS from "./SearchBar.module.css";
 
+function handleSearch() {
+  console.log("handleSearch");
+}
+
+function handleSearchChange() {
+  console.log("handleSearchChange");
+}
+
 function SearchBar() {
   return (
     <>
+    <form className={SearchBarCSS.search} onSubmit={handleSearch}>
       <input
         className={SearchBarCSS.input}
-        type="search"
+        type="text"
         placeholder="Search"
+        onChange={handleSearchChange}
       ></input>
+      <button className={SearchBarCSS.searchInput}></button>
+    </form>
     </>
   );
 }
