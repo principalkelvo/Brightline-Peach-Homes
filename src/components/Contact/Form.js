534aceb61cbc2f0d390cab169b887f4cf243a408
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-08-01 17:43:22 +0300

    submits data to backend and clears form

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -2,8 +2,15 @@ import FormCSS from "./Form.module.css";
 import React, { useState } from "react";
 import Input from "./Input";
 
-function Form(id, type, name, value, onChange, label) {
-  // console.log(id, type, name, value)
+function Form({ onAddHome }) {
+  const [rent, setRent] = useState(false);
+
+  // function handleRent(){
+  //   setRent(true);
+  // }
+  // function handleSale(){
+  //   setRent(false);
+  // }
 
   //form data
   const [input, setInput] = useState({
@@ -43,135 +50,231 @@ function Form(id, type, name, value, onChange, label) {
   const handleOnSubmit = (e) => {
     e.preventDefault();
     console.log(input);
-    console.log("input");
+    fetch("http://localhost:4000/homes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "moshi villa",
+        category: "villa",
+        image:
+          "https://res.cloudinary.com/dvcywi3uf/image/upload/v1520014434/chatbot-imoveis/ap-america.jpg",
+        forSale: !rent,
+        price: {
+          rent: 2000,
+          sale: 50000,
+        },
+        location: {
+          country: "Kenya",
+          city: "Nairobi",
+          street: "Korogocho",
+        },
+        owner: {
+          firstName: "principal",
+          lastName: "kelvin",
+          profilePic:
+            "https://res.cloudinary.com/dvcywi3uf/image/upload/v1520014276/chatbot-imoveis/casa-costa-silva.jpg",
+        },
+        description: "get your dream home today",
+        contacts: {
+          phoneNumber: "+254712345678",
+          email: "johndoe@gmail.com",
+        },
+        date: {
+          added: "2/4/2022",
+          range: 2,
+        },
+        size: input.size,
+        featured: false,
+        rating: 0,
+        variants: [
+          "https://res.cloudinary.com/dvcywi3uf/image/upload/v1520014434/chatbot-imoveis/ap-america.jpg",
+          "https://res.cloudinary.com/dvcywi3uf/image/upload/v1520014434/chatbot-imoveis/ap-america.jpg",
+          "https://res.cloudinary.com/dvcywi3uf/image/upload/v1520014434/chatbot-imoveis/ap-america.jpg",
+          "https://res.cloudinary.com/dvcywi3uf/image/upload/v1520014434/chatbot-imoveis/ap-america.jpg",
+        ],
+      }),
+    })
+      .then((r) => r.json())
+      .then((newHome) => onAddHome(newHome));
+    const submittedFormData = {
+      title: "",
+      category: "",
+      image: "",
+      sale: "",
+      rent: "",
+      country: "",
+      city: "",
+      street: "",
+      description: "",
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+      profilePic: "",
+      email: "",
+      size: "",
+      variants: ["", "", "", "", "", ""],
+    };
+    setInput(submittedFormData);
+    console.log("submitted formData", input);
+    console.log("cleared formData", submittedFormData);
   };
   //owner name, contacts, property name, category dropdown, price, location, description, size in ft,
 
   return (
     <div className={FormCSS.formContainer}>
+      <h3>Add Your Perfect Home</h3>
+      <div className={FormCSS.category}>
+        <div
+          className={
+            rent
+              ? FormCSS.categorySale
+              : FormCSS.activeCategory + " " + FormCSS.categorySale
+          }
+          onClick={() => setRent(false)}
+        >
+          for sale
+        </div>
+        <div
+          className={
+            rent
+              ? FormCSS.activeCategory + " " + FormCSS.categoryRent
+              : FormCSS.categoryRent
+          }
+          onClick={() => setRent(true)}
+        >
+          for rent
+        </div>
+      </div>
       <form onSubmit={handleOnSubmit} className={FormCSS.form}>
-        <Input
-          id="title"
-          type="text"
-          name="title"
-          value={input.title}
-          onChange={handleOnChange}
-          label="title"
-        />
-        <Input
-          id="category"
-          type="text"
-          name="category"
-          value={input.category}
-          onChange={handleOnChange}
-          label="category"
-        />
-        <Input
-          id="image"
-          type="text"
-          name="image"
-          value={input.image}
-          onChange={handleOnChange}
-          label="image"
-        />
-        <Input
-          id="sale"
-          type="text"
-          name="sale"
-          value={input.sale}
-          onChange={handleOnChange}
-          label="sale price"
-        />
-        <Input
-          id="rent"
-          type="text"
-          name="rent"
-          value={input.rent}
-          onChange={handleOnChange}
-          label="rent price"
-        />
-        <Input
-          id="country"
-          type="text"
-          name="country"
-          value={input.country}
-          onChange={handleOnChange}
-          label="country"
-        />
-        <Input
-          id="city"
-          type="text"
-          name="city"
-          value={input.city}
-          onChange={handleOnChange}
-          label="city"
-        />
-        <Input
-          id="street"
-          type="text"
-          name="street"
-          value={input.street}
-          onChange={handleOnChange}
-          label="street"
-        />
-        <Input
-          id="description"
-          type="text"
-          name="description"
-          value={input.description}
-          onChange={handleOnChange}
-          label="description"
-        />
-        <Input
-          id="firstName"
-          type="text"
-          name="firstName"
-          value={input.firstName}
-          onChange={handleOnChange}
-          label="First Name"
-        />
-        <Input
-          id="lastName"
-          type="text"
-          name="lastName"
-          value={input.lastName}
-          onChange={handleOnChange}
-          label="last Name"
-        />
+        <div className={FormCSS.inputs}>
+          <Input
+            id="title"
+            type="text"
+            name="title"
+            value={input.title}
+            onChange={handleOnChange}
+            label="title"
+          />
+          <Input
+            id="category"
+            type="text"
+            name="category"
+            value={input.category}
+            onChange={handleOnChange}
+            label="category"
+          />
+          <Input
+            id="image"
+            type="text"
+            name="image"
+            value={input.image}
+            onChange={handleOnChange}
+            label="image"
+          />
+          {rent ? (
+            <Input
+              id="rent"
+              type="text"
+              name="rent"
+              value={input.rent}
+              onChange={handleOnChange}
+              label="rent price"
+            />
+          ) : (
+            <Input
+              id="sale"
+              type="text"
+              name="sale"
+              value={input.sale}
+              onChange={handleOnChange}
+              label="sale price"
+            />
+          )}
+          <Input
+            id="size"
+            type="text"
+            name="size"
+            value={input.size}
+            onChange={handleOnChange}
+            label="size"
+          />
+          <Input
+            id="country"
+            type="text"
+            name="country"
+            value={input.country}
+            onChange={handleOnChange}
+            label="country"
+          />
+          <Input
+            id="city"
+            type="text"
+            name="city"
+            value={input.city}
+            onChange={handleOnChange}
+            label="city"
+          />
+          <Input
+            id="street"
+            type="text"
+            name="street"
+            value={input.street}
+            onChange={handleOnChange}
+            label="street"
+          />
+          <Input
+            id="description"
+            type="text"
+            name="description"
+            value={input.description}
+            onChange={handleOnChange}
+            label="description"
+          />
+          <Input
+            id="firstName"
+            type="text"
+            name="firstName"
+            value={input.firstName}
+            onChange={handleOnChange}
+            label="First Name"
+          />
+          <Input
+            id="lastName"
+            type="text"
+            name="lastName"
+            value={input.lastName}
+            onChange={handleOnChange}
+            label="last Name"
+          />
 
-        <Input
-          id="profilePic"
-          type="text"
-          name="profilePic"
-          value={input.profilePic}
-          onChange={handleOnChange}
-          label="Profile image"
-        />
-        <Input
-          id="phoneNumber"
-          type="text"
-          name="phoneNumber"
-          value={input.phoneNumber}
-          onChange={handleOnChange}
-          label="phone Number"
-        />
-        <Input
-          id="email"
-          type="text"
-          name="email"
-          value={input.email}
-          onChange={handleOnChange}
-          label="email"
-        />
-        <Input
-          id="size"
-          type="text"
-          name="size"
-          value={input.size}
-          onChange={handleOnChange}
-          label="size"
-        />
-        {/* <Input
+          <Input
+            id="profilePic"
+            type="text"
+            name="profilePic"
+            value={input.profilePic}
+            onChange={handleOnChange}
+            label="Profile image"
+          />
+          <Input
+            id="phoneNumber"
+            type="text"
+            name="phoneNumber"
+            value={input.phoneNumber}
+            onChange={handleOnChange}
+            label="phone Number"
+          />
+          <Input
+            id="email"
+            type="text"
+            name="email"
+            value={input.email}
+            onChange={handleOnChange}
+            label="email"
+          />
+
+          {/* <Input
           id="variant1"
           type="text"
           name="variants"
@@ -211,7 +314,8 @@ function Form(id, type, name, value, onChange, label) {
           onChange={handleOnChange}
           label="variant"
         /> */}
-      <button type="submit">Submit</button>
+        </div>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
